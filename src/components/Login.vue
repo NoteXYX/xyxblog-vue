00bdd45249f0a574908b657767dd8a43cb8f641c
xyx <707997228@qq.com>
2020-12-28 16:33:23 +0800

    增加登录出错提示

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
       用户名:<input type="text" v-model="loginForm.userName" placeholder="请输入用户名"/>
-      <br><br>
+      <div id="userNameTip" style="display:none; color:red">请输入用户名！</div>
+      <br>
+      <br>
       密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-      <br><br>
+      <div id="passwordTip" style="display:none; color:red">请输入密码！</div>
+      <br>
+      <br>
       <button v-on:click="login">登录</button>
   </div>
 </template>
@@ -23,6 +27,16 @@
     },
     methods: {
       login () {
+        if (this.loginForm.userName === "") {
+          document.getElementById("userNameTip").style.display="";
+        } else {
+          document.getElementById("userNameTip").style.display="none";
+        }
+        if (this.loginForm.password === "") {
+          document.getElementById("passwordTip").style.display="";
+        } else {
+          document.getElementById("passwordTip").style.display="none";
+        }
         this.$axios
           .post('/login', {
             userName: this.loginForm.userName,
@@ -31,6 +45,8 @@
           .then(successResponse => {
             if (successResponse.data.code === "SUCCESS") {
               this.$router.replace({path: '/index'})
+            } else {
+              
             }
           })
           .catch(failResponse => {
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// 设置反向代理，前端请求默认发送到 http://localhost:8081/api
+// 设置反向代理，前端请求默认发送到 http://localhost:8081/xyxblog
 var axios = require('axios')
-axios.defaults.baseURL = 'http://localhost:8081/xyxblog/api'
+axios.defaults.baseURL = 'http://localhost:8081/xyxblog'
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
