b243be731920e58e190ed864674cb2459f35d120
xieyuxin <xyxhfut@qq.com>
2020-12-29 14:01:46 +0800

    更改README.md

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # xyxblog-vue
 小谢博客前端
 
+* 同步git:
+
+  cd xyxblog-vue
+
+  git init
+
+  git pull 
+  
+  git remote set-url origin https://NoteXYX:密码@github.com/NoteXYX/xyxblog-vue.git
+
+
 * 安装模块，运行 `cnpm i`
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,12 @@
 <template>
   <el-form class="login-container" label-position="left" label-width="0px">
     <h3 class="login_title">系统登录</h3>
-
     <el-form-item>
       <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
-
     <el-form-item>
       <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
     </el-form-item>
-
     <el-form-item style="width: 100%">
       <el-button type="primary" style="width: 100%;background: #29963d;border: none" v-on:click="login">登录</el-button>
     </el-form-item>
