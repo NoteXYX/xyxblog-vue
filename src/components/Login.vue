e6b5a7a814ccfa18f7433bc0bb2eb532703cc695
xieyuxin <xyxhfut@qq.com>
2020-12-29 14:55:30 +0800

    登录页背景更新

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,20 @@
 <template>
-  <el-form class="login-container" label-position="left" label-width="0px">
-    <h3 class="login_title">系统登录</h3>
-    <el-form-item>
-      <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <el-form-item style="width: 100%">
-      <el-button type="primary" style="width: 100%;background: #29963d;border: none" v-on:click="login">登录</el-button>
-    </el-form-item>
-  </el-form>
-  <!-- <el-card>
-      用户名:<input type="text" v-model="loginForm.userName" placeholder="请输入用户名"/>
+  <body id="poster">
+    <el-form class="login-container" label-position="left" label-width="0px">
+      <h3 class="login_title">系统登录</h3>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.userName" auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
       <div id="userNameTip" style="display:none; color:red">请输入用户名！</div>
-      <br>
-      <br>
-      密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
       <div id="passwordTip" style="display:none; color:red">请输入密码！</div>
-      <br>
-      <br>
-      <button v-on:click="login">登录</button>
-  </el-card> -->
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 100%;background: #29963d;border: none" v-on:click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </body>
 </template>
 
 <script>
@@ -84,6 +77,18 @@
     margin: 0px auto 40px auto;
     text-align: center;
     color: #505458;
+  }
+
+  #poster {
+    background:url("../assets/loginBG.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
   }
 
 </style>
