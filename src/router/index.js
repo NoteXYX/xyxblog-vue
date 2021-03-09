4713871c8f2f7ba8f54bcd03407c4609b4b634de
xieyuxin <xyxhfut@qq.com>
2021-03-09 15:57:31 +0800

    图书馆页面框架done

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
 import LibraryIndex from '@/components/library/LibraryIndex'
+import ShopIndex from '@/components/shop/ShopIndex'
+import AdminIndex from '@/components/admin/AdminIndex.vue'
 
 Vue.use(Router)
 
@@ -31,6 +33,22 @@ export default new Router({
           meta: {
             requireAuth: true
           }
+        },
+        {
+          path: '/shop',
+          name: 'Shop',
+          component: ShopIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/admin',
+          name: 'Admin',
+          component: AdminIndex,
+          meta: {
+            requireAuth: true
+          }
         }
       ]
     },
@@ -38,6 +56,15 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/',
+      name: 'index',
+      redirect: '/index',
+      component: AppIndex,
+      meta: {
+        requireAuth: true
+      }
     }
   ]
 })
