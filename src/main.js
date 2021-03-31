6c8f13f0814b30a353509d8a87d0ef8fafb3b4ba
xieyuxin <xyxhfut@qq.com>
2021-03-31 21:49:03 +0800

    ok

--- src/main.js
+++ src/main.js
@@ -7,12 +7,8 @@ import store from './store'
 
 // 设置反向代理，前端请求默认发送到 http://localhost:8081/xyxblog
 var axios = require('axios')
-<<<<<<< HEAD
 // axios.defaults.baseURL = 'http://xyxblog.top:8081/xyxblog'
 axios.defaults.baseURL = process.env.API_ROOT
-=======
-axios.defaults.baseURL = 'http://81.70.147.206:8081/xyxblog'
->>>>>>> cdc60f92e35a685b4345310aa3c392802232c3f3
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
